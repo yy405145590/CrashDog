20e4ed0795f9a5efc206db1fbfba6752cbc854db
capti0n <yy405145590@sina.com>
2026-06-18 10:36:02 +0800

    123

--- start-backend-dev.bat
+++ start-backend-dev.bat
@@ -9,10 +9,10 @@ if not exist "%PYEXE%" set "PYEXE=python"
 cd /d "%ROOT%"
 echo Root: %CD%
 echo Python: %PYEXE%
-echo Mode: stable
+echo Mode: development reload
 echo.
 
-"%PYEXE%" -m uvicorn backend.main:app --host 0.0.0.0 --port 18000
+"%PYEXE%" -m uvicorn backend.main:app --host 0.0.0.0 --port 18000 --reload --reload-dir backend
 set "EXIT_CODE=%ERRORLEVEL%"
 
 echo.
